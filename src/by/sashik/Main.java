cdd632868daffd0af813ce3e679896c19eb9e923
Alexandra Kokareva <mrs.kokareva@gmail.com>
2022-03-16 00:19:32 +0300

    create gitignore and fix error

--- src/by/sashik/Main.java
+++ src/by/sashik/Main.java
@@ -9,8 +9,8 @@ public class Main {
     public static void main(String[] args) {
         Shapes shapes = new Shapes(new Shape[]{
                 new Circle(1, 1, 10),
-                new Ellipse(2, 4, 12, 7),
-                new Square(3, 5, 5)
+                new Square(3, 5, 5),
+                new Ellipse(4, 5, 10, 2)
         });
         shapes.draw();
     }
